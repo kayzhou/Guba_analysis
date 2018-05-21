07ce898e2c09d3b5edda7079d9d15f4d467a92d9
Kay Zhou <zkzhou_91@163.com>
2018-05-21 13:26:22 +0800

    types

---
+++ guba_types.py
@@ -0,0 +1,42 @@
+# 过滤信息，分为xinwen,yanbao,tweet，还有不可识别
+# 还有过滤掉无用的信息
+import os
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+in_dir = '/home/kayzhou/Project/Guba_spider/guba/data'
+in_name = '/home/kayzhou/Project/Guba_spider/guba/data/000651.txt'
+
+def get_type(content):
+    if content.startswith('来源：'):
+        return 'xinwen'
+    elif content.startswith('研报日期：'):
+        return 'yanbao'
+    else:
+        return 'tweet'
+
+
+def delete_log(t):
+    del t['download_timeout']
+    del t['depth']
+    del t['download_slot']
+    del t['download_latency']
+    return t
+
+
+for i, in_name in enumerate(os.listdir(in_dir)):
+    print(i, in_name)
+    stock_name = in_name
+    in_name = os.path.join(in_dir, in_name)
+    for j, line in enumerate(open(in_name)):
+        # print(line)
+        d = delete_log(json.loads(line))
+        if d['content']:
+            _type = get_type(d['content'])
+        else:
+            _type = 'others'
+            with open('data/{}/{}'.format(_type, stock_name), 'a') as f:
+                f.write(json.dumps(d, ensure_ascii=False) + '\n')
