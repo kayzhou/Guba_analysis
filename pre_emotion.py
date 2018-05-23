f9bf8c76d69866ac6c33e47c320866d7c27206b0
Kay Zhou <zkzhou_91@163.com>
2018-05-23 17:28:18 +0800

    update
    update

--- pre_emotion.py
+++ pre_emotion.py
@@ -1,20 +1,45 @@
+import json
+import os
+
+import jieba
+
 from emotion_cla.emo_cls import classify
 from emotion_cla.separate import separate
-
-import os
-import json
-import jieba
 
 in_dir = 'data/tweet'
 out_dir = 'data/tweet_emo'
 
-for i, in_name in enumerate(os.listdir(in_dir)):
-    print(i)
-    stock_name = in_name
-    in_name = os.path.join(in_dir, in_name)
-    for j, line in enumerate(open(in_name)):
-        d = json.loads(line)
-        d['content_pre_emo'] = classify(separate(d['content']))
-        d['title_pre_emo'] = classify(separate(d['title']))
-        with open('{}/{}'.format(out_dir, stock_name), 'a') as f:
-            f.write(json.dumps(d, ensure_ascii=False) + '\n')
+
+def pre_label():
+    '''
+    打标签
+    '''
+    for i, in_name in enumerate(os.listdir(in_dir)):
+        print(i)
+        stock_name = in_name
+        in_name = os.path.join(in_dir, in_name)
+        for j, line in enumerate(open(in_name)):
+            d = json.loads(line)
+            d['content_pre_emo'] = classify(separate(d['content']))
+            d['title_pre_emo'] = classify(separate(d['title']))
+            with open('{}/{}'.format(out_dir, stock_name), 'a') as f:
+                f.write(json.dumps(d, ensure_ascii=False) + '\n')
+
+
+def get_train_data(in_name):
+
+    for line in open(in_name):
+        d = json.loads(line.strip())
+        content = d['content']
+        title = d['title']
+        t_emo = d['title_pre_emo']
+        c_emo = d['content_pre_emo']
+        if 0 < len(content) < 200:
+            with open('data/content/{}.txt'.format(c_emo), 'a') as f:
+                f.write(str(c_emo) + '\t' + content + '\n') 
+        with open('data/title/{}.txt'.format(t_emo), 'a') as f:
+            f.write(str(t_emo) + '\t' + title + '\n') 
+        
+
+if __name__ == '__main__':
+    get_train_data('data/002446.txt')
