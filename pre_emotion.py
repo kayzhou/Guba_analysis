4086344e86ec157140cef6f01e70021eafc36e40
Kay Zhou <zkzhou_91@163.com>
2018-05-23 19:29:27 +0800

    update

--- pre_emotion.py
+++ pre_emotion.py
@@ -1,8 +1,10 @@
 import json
 import os
 import re
+import numpy as np
 
 import jieba
+import linecache
 
 from emotion_cla.emo_cls import classify
 from emotion_cla.separate import separate
@@ -11,9 +13,25 @@ in_dir = 'data/tweet'
 out_dir = 'data/tweet_emo'
 
 
+def random_ids(in_name, lens):
+    '''
+    随机选择文本的行
+    '''
+    ids = set()
+    _max = len(open(in_name).readlines())  
+    for _ in range(lens):
+        num = int(_max * np.random.random())
+        if num in ids:
+            continue
+        line = linecache.getline(in_name, num)
+        # _id = line.strip().split(',')[0]
+        ids.add(num)
+    return ids
+
+
 def pre_label():
     '''
-    打标签
+    打预标签
     '''
     for i, in_name in enumerate(os.listdir(in_dir)):
         print(i)
@@ -29,22 +47,31 @@ def pre_label():
 
 def get_train_data(in_name):
 
-    
     for line in open(in_name):
         d = json.loads(line.strip())
         content = d['content']
         title = d['title']
         t_emo = d['title_pre_emo']
         c_emo = d['content_pre_emo']
-        if not re.search('\\[\\S+\\]', title):
-            print('不满足要求')
+        
+        # 标题和内容中要有一个有表情符
+        # if not (re.search('\\[\\S+\\]', title) or re.search('\\[\\S+\\]', content)):
+        if not re.search('\\[\\S+\\]', content):
+            print('不满足要求 ...')
             continue
-        if 0 < len(content) < 200:
+
+        # 内容长度5到200
+        if 5 < len(content) < 200:
             with open('data/content/{}.txt'.format(c_emo), 'a') as f:
-                f.write(str(c_emo) + '\t' + content + '\n') 
-        with open('data/title/{}.txt'.format(t_emo), 'a') as f:
-            f.write(str(t_emo) + '\t' + title + '\n')
+                f.write(str(c_emo) + '\t' + content + '\n')
+
+        # with open('data/title/{}.txt'.format(t_emo), 'a') as f:
+        #     f.write(str(t_emo) + '\t' + title + '\n')
         
 
 if __name__ == '__main__':
-    get_train_data('data/002446.txt')
+    for line in open('data/random_ids.txt'):
+        in_name = 'data/tweet_emo/' + line.strip() + '.txt'
+        get_train_data(in_name)
+    # random_ids('data/_id.txt', 100)
+    # get_train_data('data/002446.txt')
