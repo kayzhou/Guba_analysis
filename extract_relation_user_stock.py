9f0cee02a22e133d0aca30a33778007601bbf35e
Kay <304003319@qq.com>
2018-07-25 20:04:34 +0800

    update

--- extract_relation_user_stock.py
+++ extract_relation_user_stock.py
@@ -2,6 +2,7 @@
 # 先只考虑tweet的用户
 # 另外新闻、公告、研报和其他的数据不管
 
+from tqdm import tqdm
 import json
 import os
 from collections import defaultdict
@@ -12,6 +13,8 @@ STOCK_USER = {}
 
 
 def get_relation(in_name):
+    global USER_STOCK
+    global STOCK_USER
     for line in open(in_name):
         d = json.loads(line.strip())
         uid = d['uid']
@@ -20,28 +23,27 @@ def get_relation(in_name):
         if uid not in USER_STOCK:
             USER_STOCK[uid] = defaultdict(int)
         USER_STOCK[uid][stock] += 1
-        
+
         if stock not in STOCK_USER:
            STOCK_USER[stock] = defaultdict(int)
         STOCK_USER[stock][uid] += 1
 
 
 if __name__ == '__main__':
-    in_dir = '/home/kayzhouProject/Guba_spider/data/tweet'
-    for in_name in os.listdir(in_dir):
-        if in_name.endswith('.txt'):
-            continue
+    in_dir = '/home/kayzhou/Project/Guba_spider/data/tweet/'
+    for in_name in tqdm(os.listdir(in_dir)):
         in_name = os.path.join(in_dir, in_name)
+        if not in_name.endswith('.txt'):
+            continue
         get_relation(in_name)
-        break
 
     with open('data/relation_user_stock.txt', 'w') as f:
         for uid in USER_STOCK:
-            f.write(json.dumps(USER_STOCK[uid]) + '\n')
+            f.write(uid + ' @@ ' + json.dumps(USER_STOCK[uid]) + '\n')
 
     with open('data/relation_stock_user.txt', 'w') as f:
         for stock in STOCK_USER:
-            f.write(json.dumps(STOCK_USER[stock]) + '\n')
+            f.write(stock + '@@' + json.dumps(STOCK_USER[stock]) + '\n')
 
 
 
